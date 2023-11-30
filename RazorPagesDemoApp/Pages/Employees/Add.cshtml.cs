49eab4eb9181f3b2889c76c41788c19027d5c2a8
Rafael Freire <rafaelfreirecouto@outlook.com>
2023-11-30 12:54:55 -0400

    Razor Pages Project connected to SQLServer

---
+++ RazorPagesDemoApp/Pages/Employees/Add.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesDemoApp.Data;
+using RazorPagesDemoApp.Models.Domain;
+using RazorPagesDemoApp.Models.ViewModels;
+
+namespace RazorPagesDemoApp.Pages.Employees
+{
+    public class AddModel : PageModel
+    {
+
+        private readonly RazorPagesDemoDbContext _dbContext;
+        public AddModel(RazorPagesDemoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+
+
+        [BindProperty]
+        public AddEmployeeViewModel AddEmployeeRequest { get; set; }
+
+        public void OnGet()
+        {
+            
+
+        }
+
+        public void OnPost() 
+        {
+            // Convert View Model to DomainModel
+
+
+            var employeeDomainModel = new Employee
+            {
+                Name = AddEmployeeRequest.Name,
+                Email = AddEmployeeRequest.Email,
+                Salary = AddEmployeeRequest.Salary,
+                DateOfBirth = AddEmployeeRequest.DateOfBirth,
+                Department = AddEmployeeRequest.Department
+            };
+
+            _dbContext.Employees.Add(employeeDomainModel);
+            _dbContext.SaveChanges();
+
+            ViewData["Message"] = "Employee created successsfully!";
+        }
+    }
+}
---
+++ RazorPagesDemoApp/Pages/Employees/List.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDemoApp.Pages.Employees
+{
+    public class ListModel : PageModel
+    {
+
+        public ListModel() { }
+
+
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
